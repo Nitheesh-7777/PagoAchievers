33c5573a0a6c0daa334ee1c68ebb3b92e8543e3b
Nitheeshkumar C <88040735+Nitheesh-7777@users.noreply.github.com>
2024-05-12 09:23:38 +0530

    Add files via upload

---
+++ Toast Notification in JavaScript/alert.js
@@ -0,0 +1,54 @@
+
+
+const notifications = document.querySelector(".notifications"),
+buttons = document.querySelectorAll(".buttons .btn");
+
+const toastDetails = {
+    timer: 5000,
+    success: {
+        icon: 'fa-circle-check',
+        text: 'Mesaage send successfully',
+    }
+}
+
+const removeToast = (toast) => {
+    toast.classList.add("hide");
+    if(toast.timeoutId) clearTimeout(toast.timeoutId); // Clearing the timeout for the toast
+    setTimeout(() => toast.remove(), 500); // Removing the toast after 500ms
+}
+
+const createToast = (id) => {
+    // Getting the icon and text for the toast based on the id passed
+    const { icon, text } = toastDetails[id];
+    const toast = document.createElement("li"); // Creating a new 'li' element for the toast
+    toast.className = `toast ${id}`; // Setting the classes for the toast
+    // Setting the inner HTML for the toast
+    toast.innerHTML = `<div class="column">
+                         <i class="fa-solid ${icon}"></i>
+                         <span>${text}</span>
+                      </div>
+                      <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>`;
+    notifications.appendChild(toast); // Append the toast to the notification ul
+    // Setting a timeout to remove the toast after the specified duration
+    toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
+}
+
+// Adding a click event listener to each button to create a toast when clicked
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => createToast(btn.id));
+});
+
+// Mail script
+
+
+
+function SendMail() {
+    var params ={
+    name: document.getElementById("name").value,
+    mail: document.getElementById("mail").value,
+    message: document.getElementById("message").value
+    }
+    emailjs.send("service_vbrwguh", "template_k6ju5h9", params).then(function(res) {
+    })
+    }
+  
