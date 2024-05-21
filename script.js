81fd7757c8604c7e30a28c8f99b29a5cb721d624
Nitheeshkumar C <88040735+Nitheesh-7777@users.noreply.github.com>
2024-05-21 17:40:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-// Mail script
-function SendMail() {
-  var params ={
-  name: document.getElementById("name").value,
-  mail: document.getElementById("mail").value,
-  message: document.getElementById("message").value
+function validateEmailInput() 
+{
+    let name=document.getElementById("name").value;
+    let mail=document.getElementById("mail").value;
+    let message=document.getElementById("message").value;
+
+    /*name validation */
+    if(name==="") 
+        {document.getElementById("nameFeedback").innerHTML='<span style="color:red">Please enter valid name.</span>';}
+    else
+        { document.getElementById("nameFeedback").innerHTML='';}
+    /*mail validation */
+    if(mail==="") 
+        { document.getElementById("emailFeedback").innerHTML='<span style="color:red">Provide valid email.</span>';}
+    else
+        {validateEmail()}
+    /*message validation */
+    if(message==="") 
+        { document.getElementById("messageFeedback").innerHTML='<span style="color:red">Kindly provide something else.</span>';}
+    else
+        {document.getElementById("messageFeedback").innerHTML='';}
+   
+    
+}
+
+
+function validateEmail() {
+  const emailInput = document.getElementById('mail').value;
+  const feedbackElement = document.getElementById('emailFeedback');
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailRegex.test(emailInput)) {
+    feedbackElement.textContent = ""; // Email is valid
+  } else {
+    feedbackElement.textContent = "Please enter a valid email address.";
   }
-  emailjs.send("service_vbrwguh", "template_k6ju5h9", params).then(function(res) {
-  })
-  }
+}
+
 
 
 
